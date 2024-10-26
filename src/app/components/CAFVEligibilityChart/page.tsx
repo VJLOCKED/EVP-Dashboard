ca63d3b481e1cab1a35531f39cfa6793ce92da9f
VJLOCKED <vishwanathjumanal412@gmail.com>
2024-10-27 00:23:19 +0530

    code added

--- src/app/components/CAFVEligibilityChart/page.tsx
+++ src/app/components/CAFVEligibilityChart/page.tsx
@@ -10,8 +10,7 @@ import { motion } from 'framer-motion';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-// Suppress TypeScript error for default export
-// @ts-ignore
+
 export default function CAFVEligibilityChart({ data }: { data: { 'Clean Alternative Fuel Vehicle (CAFV) Eligibility': string }[] }) {
   const eligibilityCounts = data.reduce((acc, item) => {
     const eligibility = item['Clean Alternative Fuel Vehicle (CAFV) Eligibility'] || 'Unknown';
